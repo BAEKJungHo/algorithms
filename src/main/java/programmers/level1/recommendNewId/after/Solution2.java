c470d67b3cd7f15ce8c62adc1332c21b52030621
백정호 <qorwjdgh13@mayeye.net>
2021-03-31 15:59:31 +0900

    [Programmers] 2021 KAKAO BLIND RECRUITMENT : 신규 아이디 추천

--- src/main/java/programmers/level1/recommendNewId/after/Solution2.java
+++ src/main/java/programmers/level1/recommendNewId/after/Solution2.java
@@ -14,16 +14,16 @@ public class Solution2 {
         String answer = "";
         String temp = new_id.toLowerCase();
 
-        temp = temp.replaceAll("[^-_.a-z0-9]","");
+        temp = temp.replaceAll("[^-_.a-z0-9]",""); // -_.a-z0-9 에 일치하지 않으면
         System.out.println(temp);
-        temp = temp.replaceAll("[.]{2,}",".");
-        temp = temp.replaceAll("^[.]|[.]$","");
+        temp = temp.replaceAll("[.]{2,}","."); // . 가 2번이상 반복되면
+        temp = temp.replaceAll("^[.]|[.]$",""); // . 로 시작하거나 끝나면
         System.out.println(temp.length());
         if(temp.equals(""))
             temp+="a";
         if(temp.length() >=16){
             temp = temp.substring(0,15);
-            temp=temp.replaceAll("^[.]|[.]$","");
+            temp=temp.replaceAll("^[.]|[.]$",""); // . 로 시작하거나 끝나면
         }
         if(temp.length()<=2)
             while(temp.length()<3)
