96cf91ae83119e7f0a4741866b9f521e963eb12f
BAEKJungHo <designjava@naver.com>
2021-12-02 17:07:39 +0900

    이것이코딩테스트다 - 꼬리 재귀 구현

---
+++ src/src/main/java/thisiscodingtest/chapter5/factorial/TailRecursive.java
@@ -0,0 +1,17 @@
+package thisiscodingtest.chapter5.factorial;
+
+import java.util.Scanner;
+
+public class TailRecursive {
+
+    public static int factorialTail(int n, int total) {
+        return n == 1 ? total : factorialTail(n - 1,  n * total);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(factorialTail(n, 1));
+    }
+
+}
